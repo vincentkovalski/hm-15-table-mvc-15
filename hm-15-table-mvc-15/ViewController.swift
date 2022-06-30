4dc7051bff8d32f5d49c1b91c0e8220420610a26
vincentkovalski <kovall89@gmail.com>
2022-06-30 23:41:14 +0500

    Initial Commit

---
+++ hm-15-table-mvc-15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hm-15-table-mvc-15
+//
+//  Created by Виктор Ковалевский on 30.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
