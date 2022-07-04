2b2891271930e57c6d3c328a8b34d5246a76f2c5
vincentkovalski <kovall89@gmail.com>
2022-07-04 16:00:03 +0500

    add autolayout constraint

--- hm-15-table-mvc-15/Model/SettingTableViewCell.swift
+++ hm-15-table-mvc-15/Model/SettingTableViewCell.swift
@@ -1,6 +1,7 @@
 import UIKit
 class SettingTableViewCell: UITableViewCell {
     static let identifier = "SettingTableViewCell"
+
     private let iconContainer: UIView = {
         let view = UIView()
         view.clipsToBounds = true
@@ -33,14 +34,25 @@ class SettingTableViewCell: UITableViewCell {
     override func layoutSubviews() {
         super.layoutSubviews()
         let size: CGFloat = contentView.frame.size.height - 12
-        iconContainer.frame = CGRect(x: 15, y: 6, width: size, height: size)
         let imageSize: CGFloat = size/1.5
-        iconImageView.frame = CGRect(x: (size-imageSize)/2, y: (size-imageSize)/2, width: imageSize, height: imageSize)
-        label.frame = CGRect(x: 25 + iconContainer.frame.size.width,
-                             y: 0,
-                             width: contentView.frame.size.width - 20 - iconContainer.frame.size.width,
-                             height: contentView.frame.size.height
-                             )
+
+        iconContainer.translatesAutoresizingMaskIntoConstraints = false
+        iconContainer.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 6).isActive = true
+        //iconContainer.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 6).isActive = true
+        iconContainer.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15).isActive = true
+        iconContainer.heightAnchor.constraint(equalToConstant: size).isActive = true
+        iconContainer.widthAnchor.constraint(equalToConstant: size).isActive = true
+
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.leadingAnchor.constraint(equalTo: iconContainer.trailingAnchor, constant: 10).isActive = true
+        label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 12.5).isActive = true
+        label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -12.5).isActive = true
+
+        iconImageView.heightAnchor.constraint(equalToConstant: imageSize).isActive = true
+        iconImageView.translatesAutoresizingMaskIntoConstraints = false
+                let horizontalConstraint = iconImageView.centerXAnchor.constraint(equalTo: iconContainer.centerXAnchor)
+                let verticalConstraint = iconImageView.centerYAnchor.constraint(equalTo: iconContainer.centerYAnchor)
+                NSLayoutConstraint.activate([horizontalConstraint, verticalConstraint])
     }
     override func prepareForReuse() {
         super.prepareForReuse()
